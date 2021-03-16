9300c6df237ea888c2fd9eb65ade1ffa1c6403d6
connorishall <connorishalls@gmail.com>
2021-03-16 15:04:42 -0400

    fixing +

--- Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -1,7 +1,8 @@
 var A = "This is a string.";
 window.alert(A); //This is the alert when you open up the web application
 document.write("Carlos is a cat, one day carlos said\"My name is carlos\"Some people were shocked carlos could talk."); //using quotation marks
-var hatchback = +"vw golf", sedan = +"honda accord", coupe = +"mazda miata" //trying to use variables
+var hatchback = "vw golf", sedan = "honda accord", coupe = "mazda miata" //trying to use variables
+hatchback+sedan+coupe
 var hatchback = hatchback.fontcolor("purple"); // changing the font color for the hatchback variable to purple 
 document.write(hatchback) // showing vw golf
 document.write(3+3); //using an expression
---
+++ Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -0,0 +1,5 @@
+function Button_Function()  {
+    var str = "This text is blue" ;
+    var result = str.fontcolor("blue") ; 
+    document.getElementById("Blue_Text").innerHTML = result ;
+}
