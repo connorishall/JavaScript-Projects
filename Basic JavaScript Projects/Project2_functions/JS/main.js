5aa9f07a89a7e3fdee33026d61078aee2513ee42
connorishall <connorishalls@gmail.com>
2021-03-16 15:19:10 -0400

    PROJECT2_FUNCTIONS SUBMISSION ASSIGNMENT

--- Basic JavaScript Projects/Project2_functions/JS/main.js
+++ Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -1,5 +1,11 @@
-function Button_Function()  {
-    var str = "This text is blue" ;
-    var result = str.fontcolor("blue") ; 
+function Button_Function()  { //function name
+    var str = "This text is blue" ; //string
+    var result = str.fontcolor("blue") ; //Something that is done to the string
     document.getElementById("Blue_Text").innerHTML = result ;
+}
+
+function myFunction() { //function name
+    var sentence = "I am learning"; //example of += operator
+    sentence += " a lot from this book!";
+    document.getElementById("Concatenate").innerHTML = sentence;
 }
